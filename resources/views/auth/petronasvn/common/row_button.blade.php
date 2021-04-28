d40e6a34952276af31f602ef6b535764bfdac4cc
thaivuongprimelabo <thai.vuong@primelabo.com.vn>
2021-04-28 22:31:46 +0700

    fix bug

--- resources/views/auth/petronasvn/common/row_button.blade.php
+++ resources/views/auth/petronasvn/common/row_button.blade.php
@@ -1,5 +1,9 @@
+@if(Route::has('auth_' . $name . '_remove'))
 <td align="center"><a href="javascript:void(0)" data-url="{{ route('auth_' . $name . '_remove') }}" data-id="{{ $id }}" class="remove-row" title="Remove"><i class="fa fa-trash" aria-hidden="true" style="font-size: 24px; color:#333333"></i></a></td>
+@endif
+@if(Route::has('auth_' . $name . '_edit'))
 <td align="center"><a href="javascript:void(0)" data-url="{{ route('auth_' . $name . '_edit', ['id' => $id]) }}" class="edit" title="Edit"><i class="fa fa-pencil" aria-hidden="true" style="font-size: 24px; color:#333333"></i></a></td>
-@if(isset($copy))
+@endif
+@if(Route::has('auth_' . $name . '_copy'))
 <td align="center"><a href="javascript:void(0)" data-url="{{ route('auth_' . $name . '_copy', ['id' => $id]) }}" data-id="{{ $id }}" class="edit" title="Copy"><i class="fa fa-copy" aria-hidden="true" style="font-size: 24px; color:#333333"></i></a></td>
 @endif
